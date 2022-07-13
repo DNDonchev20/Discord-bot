096786838f0cb1cce1f6a45f19e6b3224efda101
ISBachvarov21 <ISBachvarov21@codingburgas.bg>
2022-07-13 10:59:32 +0300

    Fix ban and kick
    Co-Authored-By: Deivid Donchev <85338879+DNDonchev20@users.noreply.github.com>

--- ban-kick.ts
+++ ban-kick.ts
@@ -6,41 +6,62 @@ module.exports = {
         const embed = new MessageEmbed();
         var reason = "";
         
-        if (target) {
-            const memberTarget = message.guild.members.cache.get(target.id);
-            
-            if (memberTarget.bannable) {
-                if (args[2]) {
-                    for (var i = 2; i < args.length; i++) {
-                        if (i == args.length - 1) {
-                            reason += `${args[i]}`
-
-                        }
-                        else {
-                            reason += `${args[i]} `
+        if (message.member.permissions.has("BAN_MEMBERS", true)) {
+            if (target) {
+                
+                const memberTarget = message.guild.members.cache.get(target.id);
+                
+                if (memberTarget.bannable) {
+                    if (args[2]) {
+                        for (var i = 2; i < args.length; i++) {
+                            if (i == args.length - 1) {
+                                reason += `${args[i]}`
+    
+                            }
+                            else {
+                                reason += `${args[i]} `
+                            }
                         }
                     }
+    
+                    embed.setColor("#26ff00");
+                    embed.setTitle("Success");
+                    
+                    if(reason != "")
+                    {
+                        embed.setDescription(`Successfully banned <@${target.id}> for ${reason}!`);
+                        memberTarget.send(`Banned from **${message.guild.name}** for ${reason}!`).then(() => {
+                            memberTarget.ban({"reason": reason}).catch(console.error);
+                        })
+                    }
+                    else{
+                        embed.setDescription(`Successfully banned <@${target.id}>`);
+                        memberTarget.send(`Banned from **${message.guild.name}**`).then(() => {
+                            memberTarget.ban({"reason": reason}).catch(console.error);
+                        })
+                    }
+                    
                 }
-
-                embed.setColor("#26ff00");
-                embed.setTitle("Success");
-                embed.setDescription(`Successfully banned <@${target.id}> for ${reason}!`);
-                
-                memberTarget.send(`Banned from ${message.guild.name} for ${reason}!`).then(() => {
-                    memberTarget.ban(reason);
-                })
+                else {
+                    embed.setColor("#ff0000");
+                    embed.setTitle("Error");
+                    embed.setDescription(`User <@${target.id}> can't be banned!`);
+                }
+    
             }
             else {
                 embed.setColor("#ff0000");
                 embed.setTitle("Error");
-                embed.setDescription(`User <@${target.id}> can't be banned!`);
+                embed.setDescription(`User is not found!`);
             }
-
-            message.reply({embeds: [embed]})
         }
         else {
-            message.reply("Couldn't find user!");
+            embed.setColor("#ff0000");
+            embed.setTitle("Error");
+            embed.setDescription("You don't have the required permissions do to it");
         }
+
+        message.reply({embeds: [embed]});
     },
 
     kickMember: function(message, args) {
@@ -48,43 +69,63 @@ module.exports = {
         const embed = new MessageEmbed();
         var reason = "";
 
-        if (target) {
-            const memberTarget = message.guild.members.cache.get(target.id);
-
-            if (memberTarget.kickable) {
-                if (args[2]) {
-                    for (var i = 2; i < args.length; i++) {
-                        if (i == args.length - 1) {
-                            reason += `${args[i]}`
-
-                        }
-                        else {
-                            reason += `${args[i]} `
+        if (message.member.permissions.has("KICK_MEMBERS", true)) {
+            if (target) {
+                
+                const memberTarget = message.guild.members.cache.get(target.id);
+    
+                if (memberTarget.kickable) {
+                    if (args[2]) {
+                        for (var i = 2; i < args.length; i++) {
+                            if (i == args.length - 1) {
+                                reason += `${args[i]}`
+    
+                            }
+                            else {
+                                reason += `${args[i]} `
+                            }
                         }
                     }
+                    
+                    if (reason != "") {
+                        embed.setColor("#26ff00");
+                        embed.setTitle("Success");
+                        embed.setDescription(`Successfully kicked <@${target.id}> for ${reason}!`);
+                        
+                        memberTarget.send(`Kicked from **${message.guild.name}** for ${reason}!`).then(() => {
+                            memberTarget.kick(reason);
+                        })   
+
+                    }
+                    else {
+                        embed.setColor("#26ff00");
+                        embed.setTitle("Success");
+                        embed.setDescription(`Successfully kicked <@${target.id}>`);
+                        
+                        memberTarget.send(`Kicked from **${message.guild.name}**`).then(() => {
+                            memberTarget.kick(reason);
+                        })  
+                    }
+
                 }
-                
-                embed.setColor("#26ff00");
-                embed.setTitle("Success");
-                embed.setDescription(`Successfully kicked <@${target.id}> for ${reason}!`);
-                
-                memberTarget.send(`Kicked from ${message.guild.name} for ${reason}!`).then(() => {
-                    memberTarget.kick(reason);
-                })   
+                else {
+                    embed.setColor("#ff0000");
+                    embed.setTitle("Error");
+                    embed.setDescription(`User <@${target.id}> can't be kicked!`);
+                }
             }
             else {
                 embed.setColor("#ff0000");
                 embed.setTitle("Error");
-                embed.setDescription(`User <@${target.id}> can't be kicked!`);
+                embed.setDescription(`User is not found!`);
             }
-
-            message.reply({embeds: [embed]})
+            
         }
         else {
             embed.setColor("#ff0000");
             embed.setTitle("Error");
-            embed.setDescription(`User is not found!`);
-            message.reply({embeds: [embed]})
+            embed.setDescription("You don't have the required permissions do to it");
         }
+        message.reply({embeds: [embed]})
     }
 }
